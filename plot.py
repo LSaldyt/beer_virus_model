72743a363b89d43945c51b52912466aac2364d7d
LSaldyt <lucassaldyt@gmail.com>
2020-04-02 23:11:57 -0400

    Adds geoplot

---
+++ plot.py
@@ -0,0 +1,106 @@
+from read import read
+from pprint import pprint
+
+from pandas import DataFrame
+from collections import defaultdict
+
+from urllib.request import urlopen
+
+import plotly.express as px
+
+import json, os, pickle
+
+from math import log, copysign
+
+def load_counties(filename='fips.json.cache', source='https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'):
+    if not os.path.isfile(filename):
+        with urlopen(source) as response:
+            counties = json.load(response)
+        with open(filename, 'wb') as outfile:
+            pickle.dump(counties, outfile)
+    else:
+        with open(filename, 'rb') as infile:
+            counties = pickle.load(infile)
+    return counties
+
+
+def transform():
+    regions, us = read()
+    meta = us['meta']
+
+    long_form = defaultdict(list)
+
+    for key, data in us.items():
+        if key in meta:
+            metadata = meta[key]
+            fips = metadata['fips']
+            population = int(metadata['population'])
+
+            if fips.strip() == '':
+                continue
+            if population == 0:
+                continue
+
+            county, state, *rest = key
+            data = us[key]
+
+            confirmed = data['confirmed_US']
+            dead      = data['deaths_US']
+
+            today     = confirmed[-1]
+            yesterday = confirmed[-2]
+            two_days  = confirmed[-3]
+
+            delta   = today - yesterday
+            d_delta = yesterday - two_days
+
+            if d_delta == 0:
+                change_ratio = 0.0
+            else:
+                change_ratio = delta / d_delta
+
+            percent           = today / population
+            percent_yesterday = yesterday / population
+            percent_delta     = percent - percent_yesterday
+
+            custom = change_ratio / log(population, 10)
+            
+            fips = str(int(float(fips)))
+            if len(fips) == 4:
+                fips = '0' + fips
+            long_form['county'].append(county)
+            long_form['state'].append(state)
+            long_form['fips'].append(fips)
+            long_form['today'].append(today)
+            long_form['yesterday'].append(yesterday)
+            long_form['two_days'].append(two_days)
+            long_form['dead'].append(dead[-1])
+            long_form['delta'].append(delta)
+            long_form['change_ratio'].append(change_ratio)
+            long_form['percent'].append(percent)
+            long_form['percent_yesterday'].append(percent_yesterday)
+            long_form['percent_delta'].append(percent_yesterday)
+            long_form['custom'].append(custom)
+    return DataFrame(long_form)
+
+def plot(df):
+    counties = load_counties()
+    fig = px.choropleth_mapbox(df, geojson=counties, locations='fips', color='custom',
+                               color_continuous_scale="Viridis",
+                               range_color=(0, 2.0),
+                               mapbox_style="carto-positron",
+                               zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
+                               opacity=0.4,
+                               hover_data=['county', 'state', 'today', 'yesterday', 'two_days', 'delta', 'change_ratio', 'percent', 'percent_yesterday', 'percent_delta']
+                              )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.show()
+
+def main():
+    df = transform()
+    print(df)
+    plot(df)
+
+if __name__ == '__main__':
+    main()
+
--- read.py
+++ read.py
@@ -4,10 +4,31 @@ from pprint import pprint
 import csv
 
 BASEDIR  = 'COVID-19/csse_covid_19_data/csse_covid_19_time_series/'
-BASEFILE = BASEDIR + 'time_series_19-covid-{}.csv'
-LABELS   = ['Confirmed', 'Deaths', 'Recovered']
+BASEFILE = BASEDIR + 'time_series_covid19_{}.csv'
+LABELS   = ['confirmed_global', 'confirmed_US', 'deaths_global', 'deaths_US', 'recovered_global']
 
-def read_labeled(label):
+def read_US(label):
+    regions = dict()
+    meta = dict()
+    filename = BASEFILE.format(label)
+    with open(filename, 'r') as infile:
+        label_reader = csv.reader(infile, delimiter=',', quotechar='"')
+        headers = next(label_reader)
+        dates = headers[12:]
+        for row in label_reader:
+            _, _, _, _, fips, county, state, region, lat, lng, _, pop, *counts = row
+            try:
+                counts = list(map(int, counts))
+            except ValueError:
+                print(counts)
+                print(headers)
+                print(dates)
+                print(row)
+            regions[(county, state, region)] = counts
+            meta[(county, state, region)] = dict(fips=fips, county=county, state=state, region=region, lat=lat, lng=lng, population=pop)
+    return regions, dates, meta
+
+def read_global(label):
     regions = dict()
     filename = BASEFILE.format(label)
     with open(filename, 'r') as infile:
@@ -22,18 +43,24 @@ def read_labeled(label):
                 print(headers)
                 print(row)
             regions[(province, country)] = counts
-    return regions, dates
+    return regions, dates, None
 
 def read():
     regions = defaultdict(dict)
+    us      = defaultdict(dict)
     for label in LABELS:
-        label_regions, dates = read_labeled(label)
+        reader = read_US if 'US' in label else read_global
+        output = us      if 'US' in label else regions
+        
+        label_regions, dates, meta = reader(label)
         for region_name, counts in label_regions.items():
-            regions[region_name][label] = counts
-    return regions
+            output[region_name][label] = counts
+        if meta is not None:
+            output['meta'] = meta
+    return regions, us
 
 def main():
-    pprint(read())
+    data = read()
 
 if __name__ == '__main__':
     main()
